f0e672007b53c6a71b3924b41a7c9323f6ea086e
jaba1383 <170753566+jaba1383@users.noreply.github.com>
2025-11-04 22:22:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.44.0/min/vs' }});
 
 require(['vs/editor/editor.main'], function() {
-    monaco.editor.create(document.getElementById('editor'), {
+    window.editor = monaco.editor.create(document.getElementById('editor'), {
         value: `function hello() {\n\tconsole.log("Hello, world!");\n}`,
         language: 'javascript',
         theme: 'vs-dark'
     });
-});
-window.editor = monaco.editor.create(document.getElementById('editor'), {
-  value: `function hello() {\n\tconsole.log("Hello, world!");\n}`,
-  language: 'javascript',
-  theme: 'vs-dark'
+
+    // === добавь это ===
+    window.getEditorValue = function() {
+        return window.editor ? window.editor.getValue() : "EDITOR NOT READY";
+    };
 });
